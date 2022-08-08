161d42625e29e80b483d67012da345dceadb936d
ankitmishra23 <ankit23m071996@gmail.com>
2022-08-09 02:29:49 +0530

    added queues

--- dags/students_dag.py
+++ dags/students_dag.py
@@ -13,6 +13,7 @@ from Sensors.postgresCustomSensor import customPostgresSensor
 from airflow.operators.python import BranchPythonOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.task_group import TaskGroup
+from airflow.operators.bash import BashOperator
 
 default_args={
     "start_date":datetime(2022,8,1),
@@ -29,20 +30,23 @@ with DAG('student_dag',schedule_interval='@daily',default_args=default_args,catc
         fs_conn_id='student_file_conn',
         filepath='Students_data.csv',
         poke_interval=5,
-        timeout=30
+        timeout=30,
+        queue='worker_cpu'
     )
 
     with TaskGroup(group_id='postgres_tasks') as tg1:
         create_postgres_table=PostgresOperator(
             task_id='create_postgres_table',
             postgres_conn_id='postgres_conn',
-            sql='student_ddl.sql'
+            sql='student_ddl.sql',
+            queue='worker_cpu'
         )
 
         load_to_postgres=customPostgresOperator(
             task_id='load_to_postgres',
             conn_id='postgres_conn',
             schemaname='airflow',
+            queue='worker_spark',
             filepath='/opt/airflow/datasets/Students_data.csv',
             fileschema={
             "ID":int,
@@ -69,7 +73,8 @@ with DAG('student_dag',schedule_interval='@daily',default_args=default_args,catc
         is_table_loaded=customPostgresSensor(
             task_id='is_table_loaded',
             conn_id='postgres_conn',
-            tablename='Students'
+            tablename='Students',
+            queue='worker_io'
         )
         create_postgres_table>>load_to_postgres>>is_table_loaded
 
@@ -84,11 +89,12 @@ with DAG('student_dag',schedule_interval='@daily',default_args=default_args,catc
 
     choose_action=BranchPythonOperator(
         task_id='choose_action',
-        python_callable=choose_action_based_on_sensor
+        python_callable=choose_action_based_on_sensor,
+        queue='worker_spark'
     )
 
-    processing_data=DummyOperator(task_id='processing_data')
-    table_not_found=DummyOperator(task_id='table_not_found')
+    processing_data=BashOperator(task_id='processing_data',queue='worker_io',bash_command='echo Hello',do_xcom_push=False)
+    table_not_found=BashOperator(task_id='table_not_found',queue='worker_cpu',bash_command='echo World',do_xcom_push=False)
 
     is_file_present>>tg1>>choose_action>>[processing_data,table_not_found]
 
